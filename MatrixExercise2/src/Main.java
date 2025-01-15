7e9caa16b84ef07b3f0e9c0859303c0df5dc85d1
Pedro Paulo da Silva Monteiro <109928508+PedroPaulo-Git@users.noreply.github.com>
2025-01-15 10:09:39 -0300

    Add files via upload

--- MatrixExercise2/src/Main.java
+++ MatrixExercise2/src/Main.java
@@ -32,13 +32,20 @@ public class Main {
         }
         System.out.print("find a number on matrix :" );
         int x = sc.nextInt();
+
         for(int i = 0; i < matrix.length ;i++){
             for(int j = 0 ; j < matrix[i].length ;j++){
-                System.out.print(matrix[i][j]+" ");
-                // System.out.println(j +" | "+i);
-                if (j == matrix[j].length - 1){
-                    System.out.println();
+                if (matrix[i][j] == x){
+                    System.out.println("position :" + i + " "+j );
+                    System.out.println("left :" + matrix[i][j - 1]  );
+                    System.out.println("right :" + matrix[i][j + 1]  );
+                    System.out.println("down :" + matrix[i + 1][j]  );
+
                 }
+
+
+                // System.out.println(j +" | "+i);
+
             }
         }
 
